c48eca8b32c6c6a4cbfd8987352115b085129b27
betrguy <albert.eustace97@gmail.com>
2026-01-26 19:05:37 -0500

    Quartz sync: Jan 26, 2026, 7:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The New World Grid",
+    pageTitle: "New_World_Grid",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
